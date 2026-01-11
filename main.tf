2569cdae134867a44d0d90a67e9f335ae0c9a99a
Daniel Allen <dan@aeternagroup.net>
2026-01-11 01:36:56 +0000

    Update to schema version v4.57.0

---
+++ main.tf
@@ -0,0 +1,272 @@
+resource "azurerm_container_app" "container_apps" {
+  for_each = var.container_apps
+
+  container_app_environment_id = each.value.container_app_environment_id
+  name                         = each.value.name
+  resource_group_name          = each.value.resource_group_name
+  revision_mode                = each.value.revision_mode
+  max_inactive_revisions       = each.value.max_inactive_revisions
+  tags                         = each.value.tags
+  workload_profile_name        = each.value.workload_profile_name
+
+  template {
+    dynamic "azure_queue_scale_rule" {
+      for_each = each.value.template.azure_queue_scale_rule != null ? [each.value.template.azure_queue_scale_rule] : []
+      content {
+        authentication {
+          secret_name       = azure_queue_scale_rule.value.authentication.secret_name
+          trigger_parameter = azure_queue_scale_rule.value.authentication.trigger_parameter
+        }
+        name         = azure_queue_scale_rule.value.name
+        queue_length = azure_queue_scale_rule.value.queue_length
+        queue_name   = azure_queue_scale_rule.value.queue_name
+      }
+    }
+    container {
+      args    = each.value.template.container.args
+      command = each.value.template.container.command
+      cpu     = each.value.template.container.cpu
+      dynamic "env" {
+        for_each = each.value.template.container.env != null ? [each.value.template.container.env] : []
+        content {
+          name        = env.value.name
+          secret_name = env.value.secret_name
+          value       = env.value.value
+        }
+      }
+      image = each.value.template.container.image
+      dynamic "liveness_probe" {
+        for_each = each.value.template.container.liveness_probe != null ? [each.value.template.container.liveness_probe] : []
+        content {
+          failure_count_threshold = liveness_probe.value.failure_count_threshold
+          dynamic "header" {
+            for_each = liveness_probe.value.header != null ? [liveness_probe.value.header] : []
+            content {
+              name  = header.value.name
+              value = header.value.value
+            }
+          }
+          host             = liveness_probe.value.host
+          initial_delay    = liveness_probe.value.initial_delay
+          interval_seconds = liveness_probe.value.interval_seconds
+          path             = liveness_probe.value.path
+          port             = liveness_probe.value.port
+          timeout          = liveness_probe.value.timeout
+          transport        = liveness_probe.value.transport
+        }
+      }
+      memory = each.value.template.container.memory
+      name   = each.value.template.container.name
+      dynamic "readiness_probe" {
+        for_each = each.value.template.container.readiness_probe != null ? [each.value.template.container.readiness_probe] : []
+        content {
+          failure_count_threshold = readiness_probe.value.failure_count_threshold
+          dynamic "header" {
+            for_each = readiness_probe.value.header != null ? [readiness_probe.value.header] : []
+            content {
+              name  = header.value.name
+              value = header.value.value
+            }
+          }
+          host                    = readiness_probe.value.host
+          initial_delay           = readiness_probe.value.initial_delay
+          interval_seconds        = readiness_probe.value.interval_seconds
+          path                    = readiness_probe.value.path
+          port                    = readiness_probe.value.port
+          success_count_threshold = readiness_probe.value.success_count_threshold
+          timeout                 = readiness_probe.value.timeout
+          transport               = readiness_probe.value.transport
+        }
+      }
+      dynamic "startup_probe" {
+        for_each = each.value.template.container.startup_probe != null ? [each.value.template.container.startup_probe] : []
+        content {
+          failure_count_threshold = startup_probe.value.failure_count_threshold
+          dynamic "header" {
+            for_each = startup_probe.value.header != null ? [startup_probe.value.header] : []
+            content {
+              name  = header.value.name
+              value = header.value.value
+            }
+          }
+          host             = startup_probe.value.host
+          initial_delay    = startup_probe.value.initial_delay
+          interval_seconds = startup_probe.value.interval_seconds
+          path             = startup_probe.value.path
+          port             = startup_probe.value.port
+          timeout          = startup_probe.value.timeout
+          transport        = startup_probe.value.transport
+        }
+      }
+      dynamic "volume_mounts" {
+        for_each = each.value.template.container.volume_mounts != null ? [each.value.template.container.volume_mounts] : []
+        content {
+          name     = volume_mounts.value.name
+          path     = volume_mounts.value.path
+          sub_path = volume_mounts.value.sub_path
+        }
+      }
+    }
+    cooldown_period_in_seconds = each.value.template.cooldown_period_in_seconds
+    dynamic "custom_scale_rule" {
+      for_each = each.value.template.custom_scale_rule != null ? [each.value.template.custom_scale_rule] : []
+      content {
+        dynamic "authentication" {
+          for_each = custom_scale_rule.value.authentication != null ? [custom_scale_rule.value.authentication] : []
+          content {
+            secret_name       = authentication.value.secret_name
+            trigger_parameter = authentication.value.trigger_parameter
+          }
+        }
+        custom_rule_type = custom_scale_rule.value.custom_rule_type
+        metadata         = custom_scale_rule.value.metadata
+        name             = custom_scale_rule.value.name
+      }
+    }
+    dynamic "http_scale_rule" {
+      for_each = each.value.template.http_scale_rule != null ? [each.value.template.http_scale_rule] : []
+      content {
+        dynamic "authentication" {
+          for_each = http_scale_rule.value.authentication != null ? [http_scale_rule.value.authentication] : []
+          content {
+            secret_name       = authentication.value.secret_name
+            trigger_parameter = authentication.value.trigger_parameter
+          }
+        }
+        concurrent_requests = http_scale_rule.value.concurrent_requests
+        name                = http_scale_rule.value.name
+      }
+    }
+    dynamic "init_container" {
+      for_each = each.value.template.init_container != null ? [each.value.template.init_container] : []
+      content {
+        args    = init_container.value.args
+        command = init_container.value.command
+        cpu     = init_container.value.cpu
+        dynamic "env" {
+          for_each = init_container.value.env != null ? [init_container.value.env] : []
+          content {
+            name        = env.value.name
+            secret_name = env.value.secret_name
+            value       = env.value.value
+          }
+        }
+        image  = init_container.value.image
+        memory = init_container.value.memory
+        name   = init_container.value.name
+        dynamic "volume_mounts" {
+          for_each = init_container.value.volume_mounts != null ? [init_container.value.volume_mounts] : []
+          content {
+            name     = volume_mounts.value.name
+            path     = volume_mounts.value.path
+            sub_path = volume_mounts.value.sub_path
+          }
+        }
+      }
+    }
+    max_replicas                = each.value.template.max_replicas
+    min_replicas                = each.value.template.min_replicas
+    polling_interval_in_seconds = each.value.template.polling_interval_in_seconds
+    revision_suffix             = each.value.template.revision_suffix
+    dynamic "tcp_scale_rule" {
+      for_each = each.value.template.tcp_scale_rule != null ? [each.value.template.tcp_scale_rule] : []
+      content {
+        dynamic "authentication" {
+          for_each = tcp_scale_rule.value.authentication != null ? [tcp_scale_rule.value.authentication] : []
+          content {
+            secret_name       = authentication.value.secret_name
+            trigger_parameter = authentication.value.trigger_parameter
+          }
+        }
+        concurrent_requests = tcp_scale_rule.value.concurrent_requests
+        name                = tcp_scale_rule.value.name
+      }
+    }
+    termination_grace_period_seconds = each.value.template.termination_grace_period_seconds
+    dynamic "volume" {
+      for_each = each.value.template.volume != null ? [each.value.template.volume] : []
+      content {
+        mount_options = volume.value.mount_options
+        name          = volume.value.name
+        storage_name  = volume.value.storage_name
+        storage_type  = volume.value.storage_type
+      }
+    }
+  }
+
+  dynamic "dapr" {
+    for_each = each.value.dapr != null ? [each.value.dapr] : []
+    content {
+      app_id       = dapr.value.app_id
+      app_port     = dapr.value.app_port
+      app_protocol = dapr.value.app_protocol
+    }
+  }
+
+  dynamic "identity" {
+    for_each = each.value.identity != null ? [each.value.identity] : []
+    content {
+      identity_ids = identity.value.identity_ids
+      type         = identity.value.type
+    }
+  }
+
+  dynamic "ingress" {
+    for_each = each.value.ingress != null ? [each.value.ingress] : []
+    content {
+      allow_insecure_connections = ingress.value.allow_insecure_connections
+      client_certificate_mode    = ingress.value.client_certificate_mode
+      dynamic "cors" {
+        for_each = ingress.value.cors != null ? [ingress.value.cors] : []
+        content {
+          allow_credentials_enabled = cors.value.allow_credentials_enabled
+          allowed_headers           = cors.value.allowed_headers
+          allowed_methods           = cors.value.allowed_methods
+          allowed_origins           = cors.value.allowed_origins
+          exposed_headers           = cors.value.exposed_headers
+          max_age_in_seconds        = cors.value.max_age_in_seconds
+        }
+      }
+      exposed_port     = ingress.value.exposed_port
+      external_enabled = ingress.value.external_enabled
+      dynamic "ip_security_restriction" {
+        for_each = ingress.value.ip_security_restriction != null ? [ingress.value.ip_security_restriction] : []
+        content {
+          action           = ip_security_restriction.value.action
+          description      = ip_security_restriction.value.description
+          ip_address_range = ip_security_restriction.value.ip_address_range
+          name             = ip_security_restriction.value.name
+        }
+      }
+      target_port = ingress.value.target_port
+      traffic_weight {
+        label           = ingress.value.traffic_weight.label
+        latest_revision = ingress.value.traffic_weight.latest_revision
+        percentage      = ingress.value.traffic_weight.percentage
+        revision_suffix = ingress.value.traffic_weight.revision_suffix
+      }
+      transport = ingress.value.transport
+    }
+  }
+
+  dynamic "registry" {
+    for_each = each.value.registry != null ? [each.value.registry] : []
+    content {
+      identity             = registry.value.identity
+      password_secret_name = registry.value.password_secret_name
+      server               = registry.value.server
+      username             = registry.value.username
+    }
+  }
+
+  dynamic "secret" {
+    for_each = each.value.secret != null ? [each.value.secret] : []
+    content {
+      identity            = secret.value.identity
+      key_vault_secret_id = secret.value.key_vault_secret_id
+      name                = secret.value.name
+      value               = secret.value.value
+    }
+  }
+}
+
---
+++ variables.tf
@@ -0,0 +1,347 @@
+variable "container_apps" {
+  description = <<EOT
+Map of container_apps, attributes below
+Required:
+    - container_app_environment_id
+    - name
+    - resource_group_name
+    - revision_mode
+    - template (block):
+        - azure_queue_scale_rule (optional, block):
+            - authentication (required, block):
+                - secret_name (required)
+                - trigger_parameter (required)
+            - name (required)
+            - queue_length (required)
+            - queue_name (required)
+        - container (required, block):
+            - args (optional)
+            - command (optional)
+            - cpu (required)
+            - env (optional, block):
+                - name (required)
+                - secret_name (optional)
+                - value (optional)
+            - image (required)
+            - liveness_probe (optional, block):
+                - failure_count_threshold (optional)
+                - header (optional, block):
+                    - name (required)
+                    - value (required)
+                - host (optional)
+                - initial_delay (optional)
+                - interval_seconds (optional)
+                - path (optional)
+                - port (required)
+                - timeout (optional)
+                - transport (required)
+            - memory (required)
+            - name (required)
+            - readiness_probe (optional, block):
+                - failure_count_threshold (optional)
+                - header (optional, block):
+                    - name (required)
+                    - value (required)
+                - host (optional)
+                - initial_delay (optional)
+                - interval_seconds (optional)
+                - path (optional)
+                - port (required)
+                - success_count_threshold (optional)
+                - timeout (optional)
+                - transport (required)
+            - startup_probe (optional, block):
+                - failure_count_threshold (optional)
+                - header (optional, block):
+                    - name (required)
+                    - value (required)
+                - host (optional)
+                - initial_delay (optional)
+                - interval_seconds (optional)
+                - path (optional)
+                - port (required)
+                - timeout (optional)
+                - transport (required)
+            - volume_mounts (optional, block):
+                - name (required)
+                - path (required)
+                - sub_path (optional)
+        - cooldown_period_in_seconds (optional)
+        - custom_scale_rule (optional, block):
+            - authentication (optional, block):
+                - secret_name (required)
+                - trigger_parameter (required)
+            - custom_rule_type (required)
+            - metadata (required)
+            - name (required)
+        - http_scale_rule (optional, block):
+            - authentication (optional, block):
+                - secret_name (required)
+                - trigger_parameter (optional)
+            - concurrent_requests (required)
+            - name (required)
+        - init_container (optional, block):
+            - args (optional)
+            - command (optional)
+            - cpu (optional)
+            - env (optional, block):
+                - name (required)
+                - secret_name (optional)
+                - value (optional)
+            - image (required)
+            - memory (optional)
+            - name (required)
+            - volume_mounts (optional, block):
+                - name (required)
+                - path (required)
+                - sub_path (optional)
+        - max_replicas (optional)
+        - min_replicas (optional)
+        - polling_interval_in_seconds (optional)
+        - revision_suffix (optional)
+        - tcp_scale_rule (optional, block):
+            - authentication (optional, block):
+                - secret_name (required)
+                - trigger_parameter (optional)
+            - concurrent_requests (required)
+            - name (required)
+        - termination_grace_period_seconds (optional)
+        - volume (optional, block):
+            - mount_options (optional)
+            - name (required)
+            - storage_name (optional)
+            - storage_type (optional)
+Optional:
+    - max_inactive_revisions
+    - tags
+    - workload_profile_name
+    - dapr (block):
+        - app_id (required)
+        - app_port (optional)
+        - app_protocol (optional)
+    - identity (block):
+        - identity_ids (optional)
+        - type (required)
+    - ingress (block):
+        - allow_insecure_connections (optional)
+        - client_certificate_mode (optional)
+        - cors (optional, block):
+            - allow_credentials_enabled (optional)
+            - allowed_headers (optional)
+            - allowed_methods (optional)
+            - allowed_origins (required)
+            - exposed_headers (optional)
+            - max_age_in_seconds (optional)
+        - exposed_port (optional)
+        - external_enabled (optional)
+        - ip_security_restriction (optional, block):
+            - action (required)
+            - description (optional)
+            - ip_address_range (required)
+            - name (required)
+        - target_port (required)
+        - traffic_weight (required, block):
+            - label (optional)
+            - latest_revision (optional)
+            - percentage (required)
+            - revision_suffix (optional)
+        - transport (optional)
+    - registry (block):
+        - identity (optional)
+        - password_secret_name (optional)
+        - server (required)
+        - username (optional)
+    - secret (block):
+        - identity (optional)
+        - key_vault_secret_id (optional)
+        - name (required)
+        - value (optional)
+EOT
+
+  type = map(object({
+    container_app_environment_id = string
+    name                         = string
+    resource_group_name          = string
+    revision_mode                = string
+    max_inactive_revisions       = optional(number)
+    tags                         = optional(map(string))
+    workload_profile_name        = optional(string)
+    template = object({
+      azure_queue_scale_rule = optional(object({
+        authentication = object({
+          secret_name       = string
+          trigger_parameter = string
+        })
+        name         = string
+        queue_length = number
+        queue_name   = string
+      }))
+      container = object({
+        args    = optional(list(string))
+        command = optional(list(string))
+        cpu     = number
+        env = optional(object({
+          name        = string
+          secret_name = optional(string)
+          value       = optional(string)
+        }))
+        image = string
+        liveness_probe = optional(object({
+          failure_count_threshold = optional(number, 3)
+          header = optional(object({
+            name  = string
+            value = string
+          }))
+          host             = optional(string)
+          initial_delay    = optional(number, 1)
+          interval_seconds = optional(number, 10)
+          path             = optional(string)
+          port             = number
+          timeout          = optional(number, 1)
+          transport        = string
+        }))
+        memory = string
+        name   = string
+        readiness_probe = optional(object({
+          failure_count_threshold = optional(number, 3)
+          header = optional(object({
+            name  = string
+            value = string
+          }))
+          host                    = optional(string)
+          initial_delay           = optional(number, 0)
+          interval_seconds        = optional(number, 10)
+          path                    = optional(string)
+          port                    = number
+          success_count_threshold = optional(number, 3)
+          timeout                 = optional(number, 1)
+          transport               = string
+        }))
+        startup_probe = optional(object({
+          failure_count_threshold = optional(number, 3)
+          header = optional(object({
+            name  = string
+            value = string
+          }))
+          host             = optional(string)
+          initial_delay    = optional(number, 0)
+          interval_seconds = optional(number, 10)
+          path             = optional(string)
+          port             = number
+          timeout          = optional(number, 1)
+          transport        = string
+        }))
+        volume_mounts = optional(object({
+          name     = string
+          path     = string
+          sub_path = optional(string)
+        }))
+      })
+      cooldown_period_in_seconds = optional(number, 300)
+      custom_scale_rule = optional(object({
+        authentication = optional(object({
+          secret_name       = string
+          trigger_parameter = string
+        }))
+        custom_rule_type = string
+        metadata         = map(string)
+        name             = string
+      }))
+      http_scale_rule = optional(object({
+        authentication = optional(object({
+          secret_name       = string
+          trigger_parameter = optional(string)
+        }))
+        concurrent_requests = string
+        name                = string
+      }))
+      init_container = optional(object({
+        args    = optional(list(string))
+        command = optional(list(string))
+        cpu     = optional(number)
+        env = optional(object({
+          name        = string
+          secret_name = optional(string)
+          value       = optional(string)
+        }))
+        image  = string
+        memory = optional(string)
+        name   = string
+        volume_mounts = optional(object({
+          name     = string
+          path     = string
+          sub_path = optional(string)
+        }))
+      }))
+      max_replicas                = optional(number, 10)
+      min_replicas                = optional(number, 0)
+      polling_interval_in_seconds = optional(number, 30)
+      revision_suffix             = optional(string)
+      tcp_scale_rule = optional(object({
+        authentication = optional(object({
+          secret_name       = string
+          trigger_parameter = optional(string)
+        }))
+        concurrent_requests = string
+        name                = string
+      }))
+      termination_grace_period_seconds = optional(number, 0)
+      volume = optional(object({
+        mount_options = optional(string)
+        name          = string
+        storage_name  = optional(string)
+        storage_type  = optional(string, "EmptyDir")
+      }))
+    })
+    dapr = optional(object({
+      app_id       = string
+      app_port     = optional(number)
+      app_protocol = optional(string, "http")
+    }))
+    identity = optional(object({
+      identity_ids = optional(set(string))
+      type         = string
+    }))
+    ingress = optional(object({
+      allow_insecure_connections = optional(bool, false)
+      client_certificate_mode    = optional(string)
+      cors = optional(object({
+        allow_credentials_enabled = optional(bool, false)
+        allowed_headers           = optional(list(string))
+        allowed_methods           = optional(list(string))
+        allowed_origins           = list(string)
+        exposed_headers           = optional(list(string))
+        max_age_in_seconds        = optional(number)
+      }))
+      exposed_port     = optional(number)
+      external_enabled = optional(bool, false)
+      ip_security_restriction = optional(object({
+        action           = string
+        description      = optional(string)
+        ip_address_range = string
+        name             = string
+      }))
+      target_port = number
+      traffic_weight = object({
+        label           = optional(string)
+        latest_revision = optional(bool, false)
+        percentage      = number
+        revision_suffix = optional(string)
+      })
+      transport = optional(string, "auto")
+    }))
+    registry = optional(object({
+      identity             = optional(string)
+      password_secret_name = optional(string)
+      server               = string
+      username             = optional(string)
+    }))
+    secret = optional(object({
+      identity            = optional(string)
+      key_vault_secret_id = optional(string)
+      name                = string
+      value               = optional(string)
+    }))
+  }))
+}
+
